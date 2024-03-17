fc3f25d4edfbbe774fb931a38e4e0afe80669894
Davkharbayr <davkharbayr05@gmail.com>
2024-03-18 03:03:26 +0800

    helper functions

---
+++ frontend/libs/helpers.php
@@ -0,0 +1,12 @@
+<?php 
+
+function is_post_request() : bool
+{
+    return strtoupper($_SERVER['REQUEST_METHOD']) === 'POST';
+}
+
+function is_get_request() : bool
+{
+    return strtoupper($_SERVER['REQUEST_METHOD']) === 'GET';
+}
+?>
