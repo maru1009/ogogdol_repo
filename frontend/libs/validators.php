d14e8532fd149e20c77863a95d4490b5b0b08eeb
Davkharbayr <davkharbayr05@gmail.com>
2024-03-25 03:17:16 +0800

    validation2perfection

--- frontend/libs/validators.php
+++ frontend/libs/validators.php
@@ -10,7 +10,18 @@
     }
     function check_url($url)
     {
-        preg_match("/\b(?:(?:https?|ftp):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i",$url);
+        return preg_match("/\b(?:(?:https?|ftp):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i",$url);
     }
-
+    function validate($data)
+        {
+            $data = trim($data);
+            $data = stripslashes($data);
+            $data = htmlspecialchars($data);
+            return $data;
+        }
+    function password_validate($data)
+    {
+        $pattern = '/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$/';
+        return preg_match($pattern,$data);
+    }
 ?>
--- frontend/process/reg_pro.php
+++ frontend/process/reg_pro.php
@@ -1,70 +1,118 @@
 <?php
 session_start();
 include "conn.php";
+include __DIR__ . "/../libs/validators.php";
+if($_SERVER["REQUEST_METHOD"]=="POST")
+{
 
-if (isset($_POST['uname']) && isset($_POST['password']) && isset($_POST['email'])) {
-
-    function validate($data)
+    // validating firstname
+    if(empty($_POST["uname"]))
     {
-        $data = trim($data);
-        $data = stripslashes($data);
-        $data = htmlspecialchars($data);
-        return $data;
+        header("Location: ../reg.php?error=First name is empty");
+    }
+    else{
+        if(validate_name($_POST['uname']))
+        {
+        $uname=validate($_POST['uname']);
+        }
+        else{
+            header("Location: ../reg.php?error=Not name type");
+        }
     }
 
-    $uname = validate($_POST['uname']);
-    $lname = validate($_POST["lname"]); // corrected variable name
-    $pass = validate($_POST['password']);
-    $email = validate($_POST['email']);
-
-    if (empty($uname) || empty($pass) || empty($email)) {
-        header("Location: reg.php?error=Username, password, or email is empty");
-        exit();
-    } else {
-        
-        $user_check = "SELECT * FROM customer WHERE cus_email=?";
-        $stmt_check = mysqli_prepare($conn, $user_check);
-        
-        if (!$stmt_check) {
-            die('Error in preparing check statement: ' . mysqli_error($conn));
+    // validating lastname
+    if(empty($_POST["lname"]))
+    {
+        header("Location: ../reg.php?error=Last name is empty");
+    }
+    else{
+        if(validate_name($_POST['lname']))
+        {
+        $lname=validate($_POST['lname']);
         }
-        
-        mysqli_stmt_bind_param($stmt_check, "s", $email); // corrected parameter
-        mysqli_stmt_execute($stmt_check);
-        $result = mysqli_stmt_get_result($stmt_check);
-        
-        if (mysqli_num_rows($result) > 0) {
-            header("Location: reg.php?error=User already exists");
-            exit();
-        } else {
-            
-           
-            $dynamic_salt = uniqid();
-            
-          
-            $hashed_password = hash('sha256', $pass . $dynamic_salt);
+        else{
+            header("Location: ../reg.php?error=Not name type");
+        }
+    }
 
-            $sql = "INSERT INTO customer (cus_first_name, cus_last_name, cus_email, cus_pass, pass_salt) VALUES (?, ?, ?, ?, ?)";
-            $stmt_insert = mysqli_prepare($conn, $sql);
+    // validating password
+    if(empty($_POST["password"]))
+    {
+        header("Location: ../reg.php?error=Password is empty");
+    }
+    else{
+        if(password_validate($_POST['password']))
+        {
+        $pass=validate($_POST['password']);
+        }
+        else{
+            header("Location: ../reg.php?error=Not strong password");
+        }
+    }
+
+    //validating email
+    if(empty($_POST["email"]))
+    {
+        header("Location: ../reg.php?error=Email is empty");
+    }
+    else{
+        if(validate_email($_POST['email']))
+        {
+        $email=validate($_POST['email']);
+        }
+        else{
+            header("Location: ../reg.php?error=Is not email");
+        }
+    }
+
+//  Query
+    if (isset($uname) && isset($lname) && isset($pass) && isset($email)) {
+
+            $user_check = "SELECT * FROM customer WHERE cus_email=?";
+            $stmt_check = mysqli_prepare($conn, $user_check);
             
-            if (!$stmt_insert) {
-                die('Error in preparing insert statement: ' . mysqli_error($conn));
+            if (!$stmt_check) {
+                die('Error in preparing check statement: ' . mysqli_error($conn));
             }
             
-            mysqli_stmt_bind_param($stmt_insert, "sssss", $uname, $lname, $email, $hashed_password, $dynamic_salt); 
-           
-            if (mysqli_stmt_execute($stmt_insert)) {
-                $_SESSION['username'] = $uname;
-                header('location: ../login.php');
+            mysqli_stmt_bind_param($stmt_check, "s", $email); 
+            mysqli_stmt_execute($stmt_check);
+            $result = mysqli_stmt_get_result($stmt_check);
+            // Checking if it is already registered
+            if (mysqli_num_rows($result) > 0) {
+                header("Location: reg.php?error=User already exists");
                 exit();
             } else {
-                header("Location: reg.php?error=Error executing SQL query");
-                exit();
+                
+            
+                $dynamic_salt = uniqid();
+                $hashed_password = hash('sha256', $pass . $dynamic_salt);
+
+                $sql = "INSERT INTO customer (cus_first_name, cus_last_name, cus_email, cus_pass, pass_salt) VALUES (?, ?, ?, ?, ?)";
+                $stmt_insert = mysqli_prepare($conn, $sql);
+                
+                if (!$stmt_insert) {
+                    die('Error in preparing insert statement: ' . mysqli_error($conn));
+                }
+                
+                mysqli_stmt_bind_param($stmt_insert, "sssss", $uname, $lname, $email, $hashed_password, $dynamic_salt); 
+            
+                if (mysqli_stmt_execute($stmt_insert)) {
+                    $_SESSION['username'] = $uname;
+                    header('location: ../login.php');
+                    exit();
+                } else {
+                    header("Location: reg.php?error=Error executing SQL query");
+                    exit();
+                }
             }
         }
-    }
-} else {
+    } 
+    
+else 
+{
     header("Location: reg.php?error=Invalid request");
-    exit();
+     exit();
 }
+
 ?>
