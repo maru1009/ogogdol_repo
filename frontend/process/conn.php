bc3bc8a3dae1f816260a03e9aeff799ea4a8a0f2
TserenpuntsagP <ligdenligden5@gmail.com>
2024-03-29 00:30:01 +0800

    another one

--- frontend/process/conn.php
+++ frontend/process/conn.php
@@ -1,7 +1,7 @@
 <?php
 $servername="localhost:3306";
 $username = "root";
-$password= "Y4dagd62%@d32";
+$password= "G@nb0617";
 $database= "ecommerce";
 $conn=mysqli_connect($servername,$username,$password,$database);
 
