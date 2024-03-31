05cb1904cc6b0c3b482da001c493834edf40c2ab
davkharrr <davkharbayr05@gmail.com>
2024-03-31 23:34:14 +0800

    forgot page +

--- frontend/forgot.php
+++ frontend/forgot.php
@@ -1,7 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
+ <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="css/forgot.css">
@@ -9,32 +6,15 @@
 </head>
 <body>
     <?php require_once 'assets/header.php'?>
-    <?php require 'assets/profile.php' ?>
   <!-- Password recovery form -->
-  <div class="profile-page">
-    <div class="profile-container">
-      <div class="profile-header">
-        <img src="images/image1.png" alt="Profile Picture">
-        <h2><?php echo $username; ?></h2>
-      </div>
-      <div class="profile-info">
-        <h5>Email</h5>
-        <span>Email: <?php echo $email; ?></span>
-        <h5>Phone</h5>
-        <span>Phone: <?php echo $phone; ?></span>
-      </div>
-      <div class="profile-footer">
-        <button>Edit Profile</button>
-        <button>Change Password</button>
-      </div>
+  <div class="account-page">
+    <div class="form-container">
+      <form id="ForgotPorm" action="process/forgot_pro.php" method="POST" onsubmit="return validateForm()">
+        <h2>Email-ээ оруулна уу.</h2>
+        <input type="text" placeholder="Email" required name="email" class="email-input">
+        <div class="button">
+            <a href="javascript:history.back()">Back</a>
+            <button type="submit" class="btn">Submit</button>
+        </div>
+      </form>
     </div>
-  </div>
-
-  <!-- Footer -->
-  <?php require_once 'assets/footer.php'?>
-  
-  <!-- js for toggle menu -->
-<script src="js/menu.js"></script>
-<script src="js/valid_email.js"></script>
-</body>
-</html>
--- frontend/process/forgot_pro.php
+++ frontend/process/forgot_pro.php
@@ -40,17 +40,63 @@ if(is_post_request())
         $result = mysqli_stmt_get_result($stmt_check);
 
         // Checking if it is already registered
-        if (mysqli_num_rows($result) > 0) {
-            // header("Location: ../register.php?error=User already exists");
-            $_SESSION['errors'][] = "Email is not valid";
+        if (mysqli_num_rows($result) == 1 ) {
+            
+          $token= bin2hex(random_bytes(16));
+          $token_hash= hash("sha256",$token);
+          $expiry = date("Y-m-d H:i:s", time()+ 60 * 30);
+        
+          $sql="INSERT INTO tokens (cus_email, res_token_hash, res_token_expires_at)
+                VALUES (?, ?, ?)
+                ON DUPLICATE KEY UPDATE
+                res_token_hash = VALUES(res_token_hash),
+                res_token_expires_at = VALUES(res_token_expires_at); " ;
+      
+          $stmt_check2=mysqli_prepare($conn,$sql);
+
+          if (!$stmt_check2) 
+          {
+            die('Error in preparing check statement: ' . mysqli_error($conn));
+          }
+
+          mysqli_stmt_bind_param($stmt_check2,"sss",$email,$token_hash,$expiry);
+
+          echo"aa";
+          mysqli_stmt_execute($stmt_check2);
+          echo"cc";
+          $result2=mysqli_stmt_get_result($stmt_check2);
+          echo"zl";
+          echo $result2;
+        //   $affected_row_num=mysqli_stmt_affected_rows($stmt_check2);
+          if(mysqli_affected_rows($mysql)>0)
+          {
+            echo"bb";
+            echo $result2;
+            $mailer = require  "mailer.php"; 
+           
+            $mail->setFrom("davkharbayr05@gmail.com");
+            $mail->addAddress($email);
+            $mail->Subject = "Password reset";
+            $mail->Body = <<<END
+            Click <a href="http://127.0.0.1:8083/frontend/reset-password.php?token=$token"> here </a>
+            to reset your password.
+            END;
+            $mail->send();
+            echo"Message sent";
+          }
+        
+          
+        }
+
+        // If user is not in db 
+        else{
+
+            $_SESSION['forgot_errors'][] = "Email is not valid";
             if (!empty($_SESSION['forgot_errors'])) {
                 // to-do : ene neg ymni haasha ywahig bodoh
                 header("Location: ../register.php");
                 exit();
             }
-        }
-        else{
-            
         }
     }
 }
